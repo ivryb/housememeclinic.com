bf242539f0a6b9ef089ef4431ce91b52d2a598cf
Ivan Rybnikov <ivrybn@gmail.com>
2023-09-02 20:53:01 +0300

    favicon

--- app.vue
+++ app.vue
@@ -1,4 +1,16 @@
 <template>
+  <Head>
+    <Meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <Link rel="manifest" href="/site.webmanifest" />
+    <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <Meta name="msapplication-TileColor" content="#2b5797" />
+    <Meta name="theme-color" content="#ffffff" />
+  </Head>
+
   <div data-theme="forest" class="min-h-screen flex flex-col">
     <div class="container mx-auto pt-12 pb-24 flex-1">
       <NuxtPage />
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+const title = 'Get diagnosed by House M. D. — Chat with House';
+const description = `House Meme Clinic is a web app where users can chat with the character Dr. Gregory House for a humorous diagnosis experience. It's purely for entertainment and not for medical advice. Visit the clinic and chat with House for a lighthearted medical scenario.`;
+
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+});
+</script>
+
 <template>
   <div class="prose max-w-none pt-20">
     <div>
